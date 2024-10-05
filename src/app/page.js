55db1d4f4d1e7985f1b4b3fc44a54313283ce3b4
Shekhar Suman <shekhar.suman1@in.ey.com>
2024-10-05 16:42:11 +0530

    mac build change

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import Hero from './components/Hero';
-import WelcomeBanner from './components/WelcomeBanner';
-import VideoCardPanel from './components/VideoCardPanel';
-import Screensaver from './components/Screensaver';
+import Hero from "./components/Hero";
+import WelcomeBanner from "./components/WelcomeBanner";
+import VideoCardPanel from "./components/VideoCardPanel";
+import Screensaver from "./components/Screensaver";
 
 export default function Home() {
   const handleVideoClick = (video) => {
-    console.log('Video clicked:', video);
+    console.log("Video clicked:", video);
     // Implement logic to play the video or navigate
   };
 
@@ -15,7 +15,7 @@ export default function Home() {
     <>
       <Hero />
       <WelcomeBanner />
-      <VideoCardPanel handleCardClick={handleVideoClick}/>
+      <VideoCardPanel handleCardClick={handleVideoClick} />
       <Screensaver />
     </>
   );
